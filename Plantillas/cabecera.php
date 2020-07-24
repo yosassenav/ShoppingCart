32f67ffd40e645602d746493aea2e81e5c19e202
yosassenav <mromerovanessa.soy@gmail.com>
2020-07-24 13:42:58 -0500

    Crear cabacera y pie de pagina para la vista general/Cabacera y pie de pagina para la vista general

---
+++ Plantillas/cabecera.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Tienda en linea</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a class="navbar-brand" href="index.php">Logo de la tienda</a>
+        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="my-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="mostrarCarrito.php">Carrito[<?php
+                        
+                        //  Inicializando la sesión 'CARRITO'
+                        //Si en la variable sesión 'CARRITO' está vacía se le asigna cero, 
+                        // de lo contrario se contabiliza los elementos que se le van agregando
+                        echo (empty($_SESSION['CARRITO']))?0:count($_SESSION['CARRITO']);
+                        //session_destroy();
+
+                     ?>] <span class="sr-only">(current)</span></a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+
+    <br/>
+    <br/>
+
+    <!--                 CONTAINER                      -->
+    <div class="Container" >
---
+++ Plantillas/pie.php
@@ -0,0 +1,6 @@
+<!--      END OF CONTAINER     -->
+</div>
+    <div class=" col-12 text-center"> Prueba
+    </div>
+</head>
+</html>
